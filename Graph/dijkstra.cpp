f964bba842004ebfd5bdaab8f8bcbc61dd0e3525
pushpinder001 <pushpinder.1@iitj.ac.in>
2016-07-16 13:42:59 +0530

    Add files via upload

---
+++ Graph/dijkstra.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define sz 100005
+#define M_V
+#define ii pair<int,int>
+#define vi vector<int>
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+using namespace std;
+vector<vector<ii > > gp(sz);
+vi D(sz,M_V),parent(sz,-1);
+void dijkstra(int sr)
+{
+	set<ii > s;
+	D[sr]=0;
+	s.insert(mp(0,sr));
+	while(!s.empty())
+	{
+		ii p1=*s.begin();
+		s.erase(s.begin());
+		int v=p1.s,d=p1.f;
+		for(auto i=gp[v].begin();i!=gp[v].end();i++)
+		{
+			int v2=i->f,len=i->s;
+			if(D[v]+len<D[v2])
+			{
+				if(D[v2]!=M_V)
+				{
+					s.erase(s.find(mp(D[v2],v2)));
+				}
+				D[v2]=len+D[v];
+				s.insert(mp(D[v2],v2));
+				parent[v2]=v;
+			}
+		}
+	}
+}
+int main(){
+	int n,m,l1,l2,w;
+	cin>>n>>m;
+	for(int i=0;i<m;i++)
+	{
+		cin>>l1>>l2>>w;
+		gp[l1].pb(mp(l2,w));
+		gp[l2].pb(mp(l1,w));
+	}
+	dijkstra(0);
+	cout<<endl;
+	for(int i=0;i<n;i++)
+		cout<<D[i]<<" ";
+	cout<<endl;
+	return 0;
+}
