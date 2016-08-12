f738450c8cb2a77a2549abaa677ceb5c273f539b
pushpinder001 <pushpinder.1@iitj.ac.in>
2016-08-12 12:39:31 +0530

    Update segment_tree.cpp

--- data_structure/segment_tree.cpp
+++ data_structure/segment_tree.cpp
@@ -9,21 +9,21 @@
 #define pb push_back
 typedef long long ll;
 using namespace std;
-int t[4*sz],a[sz];
+int ST[4*sz],a[sz];
 void build(int tl,int tr,int p){
 	if(tl==tr)
 	{
-		t[p]=a[tl];
+		ST[p]=a[tl];
 		return ;
 	}
 	int tm=(tl+tr)/2;
 	build(tl,tm,2*p);
 	build(tm+1,tr,2*p+1);
-	t[p]=min(t[2*p],t[2*p+1]);
+	ST[p]=min(ST[2*p],t[2*p+1]);
 }
 int query(int tl,int tr,int l,int r,int p){
 	if(tl>=l&&tr<=r)
-		return t[p];
+		return ST[p];
 	if(tr<l||tl>r)
 		return 100000;
 	int tm=(tl+tr)/2;
