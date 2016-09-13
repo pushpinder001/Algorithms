dd1218e87d48861c8d753d08fd71de2a7913a76b
pushpinder001 <pushpinder.1@iitj.ac.in>
2016-09-13 16:37:48 +0530

    Create fastIO.cpp

---
+++ Extra/fastIO.cpp
@@ -0,0 +1,24 @@
+#define gc getchar_unlocked
+#define pc(x) putchar_unlocked(x)
+int readInt() {
+  char c = gc();
+  while(c<'0' || c>'9') c = gc();
+  int ret = 0;
+  while(c>='0' && c<='9') {
+    ret = 10 * ret + c - 48;
+    c = gc();
+  }
+  return ret;
+}
+
+inline void writeInt (int n)
+{
+    int N = n, rev, count = 0;
+    rev = N;
+    if (N == 0) { pc('0'); pc('\n'); return ;}
+    while ((rev % 10) == 0) { count++; rev /= 10;} //obtain the count of the number of 0s
+    rev = 0;
+    while (N != 0) { rev = (rev<<3) + (rev<<1) + N % 10; N /= 10;}  //store reverse of N in rev
+    while (rev != 0) { pc(rev % 10 + '0'); rev /= 10;}
+    while (count--) pc('0');
+}
